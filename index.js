407a95fc877f7c5d434dc040c5690e06e1bb2a1d
Emmanuel Tukpe <tukpeemmanuel@gmail.com>
2022-05-24 18:42:56 +0100

    feat:the work is done

--- index.js
+++ index.js
@@ -1,50 +1,45 @@
-require("dotenv").config();
-const express = require("express");
-const bodyParser = require("body-parser");
-const axios = require("axios");
+const commandLineArgs = require("command-line-args");
+const commandLineUsage = require("command-line-usage");
+const { optionDefinitions, sections } = require("./command");
+const { parseHTMLPage } = require("./html");
+const { convertTextToAudio } = require("./convert");
 
-const { TOKEN, SERVER_URL } = process.env;
-const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
-const URI = `/webhook/${TOKEN}`;
-const WEBHOOK_URL = SERVER_URL + URI;
+async function run() {
+  const usage = commandLineUsage(sections);
 
-const app = express();
-app.use(bodyParser.json());
+  try {
+    const options = commandLineArgs(optionDefinitions);
 
-const init = async () => {
-  const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`);
-  console.log(res.data);
-};
+    const requiredArgs = optionDefinitions
+      .map((it) => it.name)
+      .filter((it) => it !== "help");
+    const argKeys = Object.keys(options).filter((it) => it !== "help");
 
-app.post(URI, async (req, res) => {
-  console.log(req.body);
-  const chatId = req.body.message.chat.id
-    if (req.body.message.text === '/start'){
-        await axios.post(`${TELEGRAM_API}/sendMessage`, {
-            chat_id:chatId,
-            text: "Big Mouth"
-        });
+    if (options.help || argKeys.length === 0) {
+      console.log(usage);
+      process.exit(0);
     }
 
-    else if (req.body.message.text === '/help') {
-        await axios.post(`${TELEGRAM_API}/sendMessage`, {
-            chat_id:chatId,
-            text: "The program start by converting the blogpost from the url into plain text. The text will then be passed to google-tts-api to convert the text into splitted audio files . With the help of a special package FFMPEG the splitted audio files will then be merged back into one whole file and be stored in filename given to the CLI at the start of the program."
-        });
+    const missingArgs = [];
+    for (const param of requiredArgs) {
+      if (!argKeys.includes(param)) missingArgs.push(param);
     }
-  else {
-    await axios.post(`${TELEGRAM_API}/sendMessage`, {
-        chat_id:chatId,
-        text: "Hi there"
-    });
+
+    if (missingArgs.length > 0) {
+      console.log(`missing required arguments: ${missingArgs.join(", ")}`);
+      console.log(usage);
+
+      process.exit(1);
+    }
+
+    const text = await parseHTMLPage(options["url"]);
+    await convertTextToAudio(text, options["output"]);
+  } catch (error) {
+    console.log(error.message);
+    console.log(usage);
+
+    process.exit(1);
   }
+}
 
-
-
-  return res.send();
-});
-
-app.listen(process.env.PORT || 5000, async () => {
-  console.log(`App running on port ${process.env.PORT || 5000}`);
-  await init();
-});
+run();
